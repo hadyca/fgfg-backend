0a9a9d9cc4e8cf782dd2d3508aa53b423f00733a
Jae Hyung Kim <SC425@ypsa.com>
2024-08-21 16:48:42 +0600

    0821

--- guides/seeAvailableGuides/seeAvailableGuides.resolvers.js
+++ guides/seeAvailableGuides/seeAvailableGuides.resolvers.js
@@ -4,10 +4,12 @@ export default {
   Query: {
     seeAvailableGuides: async (
       _,
-      { startTime: newStartTime, endTime: newEndTime }
+      { startTime: originalStartTime, endTime: originalEndTime }
     ) => {
       try {
-        if (!newStartTime || !newEndTime) {
+        let newEndTime = originalEndTime;
+
+        if (!originalStartTime || !newEndTime) {
           const guides = await db.guide.findMany({
             orderBy: {
               createdAt: "desc",
@@ -16,14 +18,23 @@ export default {
           return guides;
         }
 
+        // newEndTime이 "T24:00:00.000Z" 형태인지 확인
+        if (newEndTime.includes("T24:00:00.000Z")) {
+          const endDate = new Date(newEndTime);
+
+          // 하루를 더해주고 시간을 00:00으로 설정
+          endDate.setUTCDate(endDate.getUTCDate() + 1);
+          newEndTime = endDate.toISOString(); // 수정된 ISO 8601 형식으로 변환
+        }
+
         const existingReservation = await db.reservation.findMany({
           where: {
             OR: [
               {
                 // 새로운 예약의 시작 시간이 기존 예약의 시작과 종료 시간 사이에 있는 경우
                 AND: [
-                  { startTime: { lte: newStartTime } },
-                  { endTime: { gt: newStartTime } },
+                  { startTime: { lte: originalStartTime } },
+                  { endTime: { gt: originalStartTime } },
                 ],
               },
               {
@@ -36,7 +47,7 @@ export default {
               {
                 // 새로운 예약이 기존 예약을 완전히 포함하는 경우
                 AND: [
-                  { startTime: { gte: newStartTime } },
+                  { startTime: { gte: originalStartTime } },
                   { endTime: { lte: newEndTime } },
                 ],
               },
@@ -47,17 +58,20 @@ export default {
           },
           distinct: ["guideId"],
         });
+
         const guideIds = existingReservation.map((item) => item.guideId);
         const guides = await db.guide.findMany({
           where: {
-            id: { notIn: guideIds }, //중복 ID 제외
+            id: { notIn: guideIds }, // 중복 ID 제외
           },
           orderBy: {
             createdAt: "desc",
           },
         });
+
         return guides;
       } catch (error) {
+        console.error("Error in seeAvailableGuides:", error);
         return error;
       }
     },
